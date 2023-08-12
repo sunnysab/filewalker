0b4e77ea5a17ba088ca4bf6af08dfccf24e4758e
sunnysab <i@sunnysab.cn>
2023-08-12 19:08:01 +0800

    Add file-only mode support.

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,7 @@
 use std::fs::{DirEntry, ReadDir};
-use std::path::Path;
 use std::io;
 use std::os::unix::ffi::OsStrExt;
-
+use std::path::Path;
 
 /// Iterator over the entries in a directory and its sub-directories.
 pub struct FileWalker {
@@ -12,6 +11,8 @@ pub struct FileWalker {
 
     /// Hidden file flag
     filter_hidden_files: bool,
+    /// Iter on file only
+    file_only: bool,
 }
 
 impl FileWalker {
@@ -19,13 +20,19 @@ impl FileWalker {
         let iter = root.read_dir()?;
 
         let stack = vec![iter];
-        Ok(FileWalker { stack, filter_hidden_files: false })
+        Ok(FileWalker { stack, filter_hidden_files: false, file_only: false })
     }
 
     /// Filter out hidden files and directories.
     /// Note: this function may cause performance loss.
     pub fn filter_hidden_items(mut self, flag: bool) -> Self {
         self.filter_hidden_files = flag;
+        self
+    }
+
+    /// Iter on file only, filter out pipe, tty, ...
+    pub fn file_only(mut self, flag: bool) -> Self {
+        self.file_only = flag;
         self
     }
 
@@ -36,25 +43,31 @@ impl FileWalker {
             // If current layer's iterator is available, read next file
             for next_entry in lowest_dir_iter {
 
-                    // If current entrance is not available, consider that there are still some files to
-                    // iterate and `lowest_dir_iter` has been moved, return error and wait for a next call.
-                    let entry = next_entry?;
+                // If current entrance is not available, consider that there are still some files to
+                // iterate and `lowest_dir_iter` has been moved, return error and wait for a next call.
+                let entry = next_entry?;
 
-                    if self.filter_hidden_files {
-                        let file_name = &entry.file_name();
-                        if let [0x2E, _rest @ ..] = file_name.as_bytes() {
-                            continue;
-                        }
+                if self.filter_hidden_files {
+                    let file_name = &entry.file_name();
+                    if let [0x2E, _rest @ ..] = file_name.as_bytes() {
+                        continue;
                     }
+                }
 
-                    if entry.file_type()?.is_dir() {
-                        // Once we found a new directory, push its iterator back to the stack
-                        // If error occures, maybe for permission denied the sub-directory can not be accessed,
-                        // just return that error and ignore, wait for a next call
-                        let new_subdirectory = entry.path().read_dir()?;
-                        self.stack.push(new_subdirectory);
-                        break 'iter_dir;
-                    }
+                let file_type = entry.file_type()?;
+                if file_type.is_dir() {
+                    // Once we found a new directory, push its iterator back to the stack
+                    // If error occures, maybe for permission denied the sub-directory can not be accessed,
+                    // just return that error and ignore, wait for a next call
+                    let new_subdirectory = entry.path().read_dir()?;
+                    self.stack.push(new_subdirectory);
+                                        // Leave current directory and jump to subdirectory.
+                    // When the sub task end, this level of directory continue.
+                    break 'iter_dir;
+                } else if (self.file_only && file_type.is_file()) || !self.file_only{
+                    return Ok(Some(entry));
+                }
+                // current file is not a regular file, continue.
             }
             // Return to up level and try again
             self.stack.pop();
